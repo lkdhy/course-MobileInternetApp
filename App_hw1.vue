8f233ab07f2a56b9782bd630a4c37b02161dd74f
lkdhy <li2003xfkl@163.com>
2024-04-26 09:25:31 +0800

    remove其它上次demo而增加的无用东西

---
+++ App_hw1.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div id="one">score:{{score}}</div>
+    <h2>Record:{{Record}}</h2>
+    <button @click="countTime">测试</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+    name:"App",
+    data(){
+        return {
+            score:0,
+            times:0,
+            start: '',
+            stop: '',
+            Record: 0
+        }
+    },
+    methods:{
+        countTime(){
+    
+            
+            if(this.times%2 === 0){
+                this.start = new Date();
+                this.Record = 0
+            }
+            else{
+                this.stop = new Date();
+                this.Record = Number(this.stop.getTime()) - Number(this.start.getTime());
+            }
+            this.times++;
+            if(this.score < this.Record && this.Record <= 1000){
+                this.score = this.Record
+            }
+        }
+    },
+    components:{
+        Record(){
+
+        }
+    }
+}
+</script>
+
+<style>
+    #one {
+        background-color: yellow;
+        text-align: center;
+        color: blue;
+    }
+</style>
---
+++ background-slider-swiper-js/background-slider-swiper-js/dist/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".swiper", {
+  grabCursor: true,
+  speed: 500,
+  effect: "slide",
+  loop: true,
+  mousewheel: {
+    invert: true,
+    sensitivity: 1,
+  },
+});
+
+swiper.enable();
