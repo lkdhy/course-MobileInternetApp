bd6016d55f9756fa4b071ae8d794317d3be77418
lkdhy <li2003xfkl@163.com>
2024-05-01 16:36:17 +0800

    用npm create vue@latest构建了初始vue项目

---
+++ Vue3_test/button_compAPI.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {ref, onMounted} from "vue";
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+</script>
+
+<template>
+  <button @click="increment">"Count is: " {{ count }}</button>
+
+</template>
+
+<style scoped>
+
+</style>
